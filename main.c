a6132c8287f21367248c0f70c96bdf5412bd5f67
Ryan Joshua <mail@ryanjoshuagapac.com>
2026-05-24 17:39:56 +0800

    show overlay on top; todo: draw the screen grab

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/extensions/XShm.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/shm.h>
@@ -19,6 +20,11 @@ int main() {
   XWindowAttributes wa;
   XGetWindowAttributes(d, root, &wa);
   int width = wa.width, height = wa.height;
+
+  XSetWindowAttributes attr;
+  attr.override_redirect = True;
+  attr.background_pixel = BlackPixel(d, screen);
+  attr.event_mask = KeyPressMask;
 
   if (!XShmQueryExtension(d)) {
     fprintf(stderr, "XShm extension not supported!\n");
@@ -46,8 +52,33 @@ int main() {
 
   XShmGetImage(d, root, img, 0, 0, AllPlanes);
 
-  // TODO: overlay the whole thing to the screen
+  Window overlay = XCreateWindow(
+      d, root, 0, 0, width, height, 0, CopyFromParent, InputOutput,
+      CopyFromParent, CWOverrideRedirect | CWBackPixel | CWEventMask, &attr);
+  XMapWindow(d, overlay);
 
+  while (XGrabKeyboard(d, overlay, 1, GrabModeAsync, GrabModeAsync,
+                       CurrentTime) != GrabSuccess) {
+    struct timespec ts = {0, 10000000L};
+    nanosleep(&ts, NULL);
+  }
+
+  XEvent ev;
+  int running = 1;
+  while (running) {
+    XNextEvent(d, &ev);
+
+    if (ev.type == KeyPress) {
+      KeySym key = XKeycodeToKeysym(d, ev.xkey.keycode, 0);
+
+      if (key == XK_q || key == XK_Escape) {
+	running = 0;
+      }
+    }
+  }
+
+  XUngrabKeyboard(d, CurrentTime);
+  XDestroyWindow(d, overlay);
   XShmDetach(d, &shminfo);
   XDestroyImage(img);
   shmdt(shminfo.shmaddr);
